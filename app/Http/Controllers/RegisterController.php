da3f8e212ec24870b52b012647543340d8184d50
fauziahmad37 <fauzi.enginer@gmail.com>
2022-01-29 09:59:34 +0700

    pembuatan backend register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,4 +15,24 @@ class RegisterController extends Controller
             'active' => 'register'
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:100',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        // $request->session()->flash('success', 'Registration successfull! Please Login');
+
+        return redirect('/login')->with('success', 'Registration successfull! Please Login');
+    }
+    
 }
